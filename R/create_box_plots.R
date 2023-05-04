8d5658fc015ebf65cd970377ee2a080fb9570286
ScottFB101 <113871715+ScottFB101@users.noreply.github.com>
2023-05-04 13:55:39 -0700

    Starting box plots function

---
+++ R/create_box_plots.R
@@ -0,0 +1,21 @@
+#' Creates themed box plots from a raw dataframe
+#'
+#' @param data data.frame
+#'
+#'
+#' @return A ggplot object
+#'
+#'
+#'
+#'
+create_box_plots <- function(data) {
+
+  ggplot(data, aes(x = category, y = value, fill = category)) +
+    geom_boxplot() +
+    scale_fill_hue(name = "Category") +
+    labs(title = "Box Plot Example", x = "Category", y = "Value") +
+    theme_minimal()
+
+}
+# Helper function
+#Summarize function...
